7f629dcddabe169319fac8a31804856616510ab2
wpk-nist-gov <wpk@nist.gov>
2026-03-24 17:24:50 -0400

    chore: update copier and requirements (#38)

--- docs/conf.py
+++ docs/conf.py
@@ -103,10 +103,8 @@ myst_substitutions = {
 # ]
 
 myst_url_schemes = ("http", "https", "mailto")
-
 nb_execution_mode = "cache"
 # nb_execution_mode = "auto"
-
 # set the kernel name
 nb_kernel_rgx_aliases = {"lnpy.*": "python3", "conda.*": "python3"}
 
@@ -114,6 +112,7 @@ nb_execution_allow_errors = True
 
 # Whether to remove stderr
 nb_output_stderr = "remove"
+
 
 # - top level variables --------------------------------------------------------
 # set github_username variable to be subbed later.
